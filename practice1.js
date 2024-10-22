e2c8ce4d7e855d56703d1ac9ffb4d08aa31f0e6b
macharlasathya <sathishmacharla5995@gmail.com>
2024-10-22 12:14:16 +0530

    adding new files into new repository

---
+++ practice1.js
@@ -0,0 +1,115 @@
+// var age=23;
+// if(age>=18)
+// {
+//     console.log("your elgible fo the vote")
+// }
+//  var birthday=2000;
+//  if(birthday>=3000)
+//  {
+//     console.log("appy birth")
+//  }
+//  var a=0
+//  if(a>-1)
+//  {
+//     console.log("hiii")
+//  }
+//  else{
+//     console.log("hkloo")
+//  }
+//  var a1="";
+//  var a2="";
+//  if(a1==a2)
+//  {
+//     console.log("hii")
+//  }
+//  else
+//  {
+//     console.log("oooo")
+
+//  }
+//  if("redcolor"&&"hii"&&"gjjj")
+//  {
+//     console.log("hii")
+//  }
+//  else{
+//     console.log("kkk")
+//  }
+//  var a=20;
+//  if(a %2 === 0)
+//  {
+// console.log("evene nuber")
+//  }
+//   var b=40;
+//   if(b>10)
+//   {
+// console.log("yes")
+//   }
+//   else if(b>40)
+//   {
+// console.log("wow")
+//   }
+//   else
+//   {
+// console.log("try again")
+//   }
+ 
+  
+  //  var marks=76;
+  //  if(marks>90)
+  //  {
+  //   console.log("A+")
+  //  }
+  //  else if(marks>=81 && marks<=90)
+  //  {
+  //  console.log("A")
+  //  }
+  //  else if(marks>=71 && marks<=80)
+  //  {
+  //   console.log("B+")
+  //  }
+  //  else if(marks>=61 && marks<=70)
+  //  {
+  //   console.log("c+")
+  //  }
+  //  else if(marks>=51 && marks<=60)
+  //  {
+  //    console.log("c")
+  //  }
+  //  else if(marks>=40 && marks<=50)
+  //  {
+  //   console.log("D")
+  //  }
+  //  else if(marks<50)
+  //  {
+  //   console.log("FAil")
+  //  }
+
+  //  var marks=76;
+  //  if(marks>=90)
+  //  {
+  //   console.log("A+")
+  //  }
+  //  else if(marks>=81)
+  //  {
+  //  console.log("A")
+  //  }
+  //  else if(marks>=71)
+  //  {
+  //   console.log("B+")
+  //  }
+  //  else if(marks>=61)
+  //  {
+  //   console.log("c+")
+  //  }
+  //  else if(marks>=51)
+  //  {
+  //    console.log("c")
+  //  }
+  //  else if(marks>=40)
+  //  {
+  //   console.log("D")
+  //  }
+  //  else if(marks<50)
+  //  {
+  //   console.log("FAil")
+  //  }
---
+++ task1.js
@@ -0,0 +1,50 @@
+var age=22;
+ if(age>18)
+ {
+console.log("eligible for vote")
+ }                                            //quetsion1//
+ else
+ {
+console.log("not eligible")
+ }
+
+ let wheather="rainy"
+ if(wheather=="rainy")
+ {
+  console.log("its pouring outside")
+ }                                                   //question2//
+ else
+ {
+console.log("not raining")
+ }
+
+
+ var marks=76;
+   if(marks>90)
+   {
+    console.log("A+")
+   }
+   else if(marks>=81 && marks<=90)
+   {
+   console.log("A")
+   }
+   else if(marks>=71 && marks<=80)                    //question3//
+   {
+    console.log("B+")
+   }
+   else if(marks>=61 && marks<=70)
+   {
+    console.log("c+")
+   }
+   else if(marks>=51 && marks<=60)
+   {
+     console.log("c")
+   }
+   else if(marks>=40 && marks<=50)
+   {
+    console.log("D")
+   }
+   else if(marks<50)
+   {
+    console.log("FAil")
+   }
